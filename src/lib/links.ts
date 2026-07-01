22146dd270db3aac475041a666416b08596d8227
Ricardo Araque <63428864+reussy@users.noreply.github.com>
2026-06-30 20:44:38 -0600

    Update links.ts
    Add Gumroad link

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,3 +1,3 @@
 // Central place to edit outbound links.
 // TODO: Reemplaza esta URL por el checkout real del Programa Portafolio Funcional.
-export const PROGRAMA_URL = "https://abcfinanciero.com/programa";
+export const PROGRAMA_URL = "https://gum.co/u/cw9lst09";
